2730a687378de32d71a56d31b8bbc24a0da77d12
admin <admin@admins-MacBook-Pro.local>
2023-07-16 20:57:52 +0530

    api added : Save External Request

--- elevator_system/apis/save_request.py
+++ elevator_system/apis/save_request.py
@@ -16,31 +16,88 @@ class SaveUserInternalRequestView(APIView):
             return Response({'error': 'Elevator in Maintainance.'}, status=status.HTTP_404_NOT_FOUND)
 
         requested_floor = request.data.get('floor')
+        return elevator_trigger(elevator, requested_floor)
 
-        requests = elevator.requests.filter(status='PENDING', floor=requested_floor)
-        if len(requests) != 0:
-            serializer = RequestSerializer(requests)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class SaveUserExternalRequestView(APIView):
+    def get_elevator(requested_floor):
+        elevators = Elevator.objects.filter(is_operational=True)
 
+        optimal_elevator = None
+        if len(elevator)==0:
+            return optimal_elevator
+        
+        optimal_elevator = elevators[0]
+        max_distance = -1
 
-        requests = elevator.requests.filter(status='PENDING')
-        request_obj = Request.objects.create(elevator=elevator, floor=requested_floor)
+        for elevator in elevators:
+            current_floor = elevator.current_floor
+            current_direction = elevator.current_direction
 
-        if elevator.is_open == True:
-            if len(requests) == 0:
-                if elevator.current_floor > requested_floor:
-                    elevator.current_direction = "DOWN"
+            if current_direction == 'IDLE':
+                distance = abs(requested_floor - current_floor)
+            elif current_direction == 'UP':
+                if current_floor > requested_floor:
+                    distance = current_floor - requested_floor
                 else:
-                    elevator.current_direction = "UP"
-                elevator.save()
-        else:
-            if len(requests) == 0:
-                with transaction.atomic():
-                    request_obj.status = 'COMPLETED'
-                    request_obj.save()
-                    elevator.current_floor = requested_floor
-                    elevator.is_open = True
-                    elevator.save()
+                    max_requested_floor = elevator.requests.filter(status='PENDING', floor__gt=current_floor).order_by('-floor').first()
+                    if max_requested_floor:
+                        distance = max_requested_floor.floor - current_floor + max_requested_floor.floor - requested_floor
+                    else:
+                        distance = current_floor - requested_floor
+            else:
+                if current_floor < requested_floor:
+                    distance = requested_floor - current_floor
+                else:
+                    max_requested_floor = elevator.requests.filter(status='PENDING', floor__lt=current_floor).order_by('floor').first()
+                    if max_requested_floor:
+                        distance = current_floor - max_requested_floor.floor + requested_floor - max_requested_floor.floor
+                    else:
+                        distance = requested_floor - current_floor
 
-        serializer = RequestSerializer(request_obj)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            if distance > max_distance:
+                max_distance = distance
+                optimal_elevator = elevator
+
+        return optimal_elevator
+    
+    def post(self, request, elevator_id):
+
+        requested_floor = request.data.get('floor')
+        elevator = self.get_elevator(requested_floor)
+
+        if elevator==None:
+            return Response({'error': 'Elevator in Maintainance.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        return elevator_trigger(elevator, requested_floor)
+
+
+
+def elevator_trigger(elevator, requested_floor):
+    requests = elevator.requests.filter(status='PENDING', floor=requested_floor)
+    if len(requests) != 0:
+        serializer = RequestSerializer(requests)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    requests = elevator.requests.filter(status='PENDING')
+    request_obj = Request.objects.create(elevator=elevator, floor=requested_floor)
+
+    if elevator.is_open == True:
+        if len(requests) == 0:
+            if elevator.current_floor > requested_floor:
+                elevator.current_direction = "DOWN"
+            else:
+                elevator.current_direction = "UP"
+            elevator.save()
+    else:
+        if len(requests) == 0:
+            with transaction.atomic():
+                request_obj.status = 'COMPLETED'
+                request_obj.save()
+                elevator.current_floor = requested_floor
+                elevator.is_open = True
+                elevator.save()
+
+    serializer = RequestSerializer(request_obj)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
--- elevator_system/urls.py
+++ elevator_system/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .apis.initialize import ElevatorInitializationView
-from .apis.save_request import SaveUserInternalRequestView
+from .apis.save_request import SaveUserInternalRequestView, SaveUserExternalRequestView
 from .apis.direction import CurrentDirectionView, NextDestinationView
-from .apis.elevator_status import SetElevatorOperationalView
 from .apis.elevator_status import SetElevatorOperationalView
 from .apis.get_request import ElevatorRequestsView
 
@@ -13,5 +12,6 @@ urlpatterns = [
     path('api/elevator/<int:elevator_id>/next_destination/', NextDestinationView.as_view(), name='next_destination'),
     path('api/elevator/<int:elevator_id>/current_direction/', CurrentDirectionView.as_view(), name='current_direction'),
     path('api/elevator/<int:elevator_id>/save_request/', SaveUserInternalRequestView.as_view(), name='save_request'),
+    path('api/elevator/call_elevator', SaveUserExternalRequestView.as_view(), name='call_elevator'),
     path('api/elevator/<uuid:elevator_id>/set_operational/', SetElevatorOperationalView.as_view(), name='set_operational'),
 ]
